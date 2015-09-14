d92e2a9478cefac5c2908ac47a94abddda5f1204
Gerob Kimball <gerobk@gmail.com>
2015-09-14 14:26:14 -0400

    Fix final invoice page to only show requested information

--- resources/views/orders/new/confirm.blade.php
+++ resources/views/orders/new/confirm.blade.php
@@ -16,26 +16,14 @@
             </div>
 
             <div class="row">
-                <div class="col-md-6">
+                <div class="col-md-12">
                     <h4>Customer Information</h4>
 
                     <strong>First Name: </strong> {{ $order->first_name }} <br>
                     <strong>Last Name: </strong> {{ $order->last_name }} <br>
                     <strong>Member Number: </strong> {{ $order->member_number }} <br>
-                </div>
-                <div class="col-md-6">
-                    <h4>Device Information</h4>
-
-                    <strong>Device: </strong> {{ $order->device_name }} <br>
-                    <strong>Color: </strong> {{ $order->color }} <br>
-                    <strong>Carrier: </strong> {{ $order->carrier }} <br>
-                    <strong>Claim: </strong> {{ $order->claim == 'on' ? 'Square Trade' : '' }} <br>
                     <strong>Claim Number: </strong> {{ $order->claim_number }} <br>
-                    <strong>Description: </strong> {{ $order->description }} <br>
-
-                    <h4>Order Information</h4>
-                    <strong>Store Number</strong> {{ $order->store_number }} <br>
-                    <strong>Technician Initials</strong> {{ $order->technician_initials }} <br>
+                    <br>
                 </div>
             </div>
             <div class="row">
